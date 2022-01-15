3bee39d7da81b76c7fb68c95ea6825afd4eb887b
sujithHubpost <sujithx007@gmail.com>
2022-01-15 15:34:32 +0530

    First

---
+++ run.sh
@@ -0,0 +1,18 @@
+
+echo 'blacklist r8188eu'|sudo tee -a '/etc/modprobe.d/realtek.conf'
+
+OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
+echo "OS Version is $OS_VERSION"
+
+cd output
+mod_dir="example_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+for each in $mod_dir; do
+	echo Loading module from "$each"
+	insmod "$each/hello.ko"
+	lsmod | grep hello
+	rmmod hello
+done
+
+while true; do
+	sleep 60
+done
